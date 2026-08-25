c371283b1b28c87bb589b65c43cf2fb16f256ccd
Pominus <pominuseventguard@gmail.com>
2026-08-25 21:04:18 +0200

    premier migration

--- Backend/src/db/schema.js
+++ Backend/src/db/schema.js
@@ -2,8 +2,9 @@ import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
-    email: text('email').notNullable().unique(),
-    username: text('username').notNullable(),
-    createdAt: timestamp('created_at').defaultNow().notNullable(),
-    updatedAt: timestamp('updatedAt').defaultNow().notNullable()
+    email: text('email').notNull().unique(),
+    username: text('username').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt').defaultNow().notNull()
 });
+
