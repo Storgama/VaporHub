b4872a2db9be569fe07ae06459b7f8cb5770fe04
Pominus <pominuseventguard@gmail.com>
2026-08-27 20:08:31 +0200

    Ajout du middleware pour la gestion des futurs droits + remaniment des fichiers pour les BDD

--- Backend/drizzle.config.js
+++ Backend/drizzle.config.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 export default {
-  schema: './src/db/schema.js',
+  schema: './src/db/schema/index.js',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
--- Backend/src/db/schemas/users.js
+++ Backend/src/db/schemas/users.js
@@ -4,6 +4,8 @@ export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
     email: text('email').notNull().unique(),
     username: text('username').notNull(),
+    password: text('password').notNull(),
+    role: text('role').default('user').notNullable(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updatedAt').defaultNow().notNull()
 });
